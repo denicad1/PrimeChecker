492348626ee87a2397437378e9a00b3f3f773161
Anthony Denicolo <denicoloa@live.bucks.edu>
2018-07-17 15:40:51 -0400

    will check to see if a number is prime or not prime.

--- PrimeChecker/js/main.js
+++ PrimeChecker/js/main.js
@@ -1,1 +1,19 @@
-﻿// Your code here!
+﻿
+function divide(n) {
+    var answer = 0;
+    for (var i = 2; i < n; i++) {
+        if (n % i >= 1) {
+            answer=1;
+        }else {
+            answer = 0;
+            break;
+        }
+       
+    }
+    if (answer>0) {
+        return "prime";
+    } else {
+        return "not prime";
+    }
+}
+console.log(divide(27));
